18d30344943e76cc381bd5274b1c9486db0d88cb
Riya Uppal <ruppal@fb.com>
2017-07-11 10:29:36 -0700

    Initial commit

---
+++ app/src/main/java/com/ruppal/orbz/LoginOtherActivity.java
@@ -0,0 +1,13 @@
+package com.ruppal.orbz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginOtherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_other);
+    }
+}
