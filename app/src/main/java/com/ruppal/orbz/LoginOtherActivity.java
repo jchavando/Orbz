7959975c4f16e430964636e2962fdea8a0aee045
jchavando <jchavand@stanford.edu>
2017-07-11 13:21:46 -0700

    LoginOtherActivity.java modified to support Spotify login

--- app/src/main/java/com/ruppal/orbz/LoginOtherActivity.java
+++ app/src/main/java/com/ruppal/orbz/LoginOtherActivity.java
@@ -1,16 +1,88 @@
 package com.ruppal.orbz;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
-public class LoginOtherActivity extends AppCompatActivity {
-    String hello = "hello";
-    String riya = "Riya";
-    String testing = "testing";
-    String Elvis = "Elvis";
+import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Error;
+import com.spotify.sdk.android.player.Player;
+import com.spotify.sdk.android.player.PlayerEvent;
+import com.spotify.sdk.android.player.SpotifyPlayer;
+
+public class LoginOtherActivity extends AppCompatActivity implements SpotifyPlayer.NotificationCallback, ConnectionStateCallback {
+
+    // TODO: Replace with your client ID
+    private static final String CLIENT_ID = "de5914f8fc0a4f40b20266a180442c79";
+    // TODO: Replace with your redirect URI
+    private static final String REDIRECT_URI = "adrenainejunkies://callback";
+
+    private Player mPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_other);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onPlaybackEvent(PlayerEvent playerEvent) {
+        Log.d("LoginOtherActivity", "Playback event received: " + playerEvent.name());
+        switch (playerEvent) {
+            // Handle event type as necessary
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onPlaybackError(Error error) {
+        Log.d("LoginOtherActivity", "Playback error received: " + error.name());
+        switch (error) {
+            // Handle error type as necessary
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onLoggedIn() {
+        Log.d("LoginOtherActivity", "User logged in");
+    }
+
+    @Override
+    public void onLoggedOut() {
+        Log.d("LoginOtherActivity", "User logged out");
+    }
+
+    @Override
+    public void onLoginFailed(Error error) {
+        Log.d("LoginOtherActivity", "Login failed");
+    }
+
+//    @Override
+//    public void onLoginFailed(int i) {
+//        Log.d("MainActivity", "Login failed");
+//    }
+
+    @Override
+    public void onTemporaryError() {
+        Log.d("LoginOtherActivity", "Temporary error occurred");
+    }
+
+    @Override
+    public void onConnectionMessage(String message) {
+        Log.d("LoginOtherActivity", "Received connection message: " + message);
     }
 }
